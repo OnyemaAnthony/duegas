34ac49f81f21b15d40776b5d5cfc80961a26b7b7
OnyemaAnthony <Anthonychibuike246@gmail.com>
2025-08-02 17:50:49 +0100

    clean up

--- lib/main.dart
+++ lib/main.dart
@@ -9,44 +9,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-//
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//   runApp(
-//     const MyApp(),
-//   );
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     final user = Provider.of<AuthRepository?>(context);
-//
-//     return MultiProvider(
-//       providers: [
-//         ChangeNotifierProvider(
-//             create: (_) => AuthenticationProvider(AuthRepository(
-//                 firebaseAuth: FirebaseAuth.instance,
-//                 firestore: FirebaseFirestore.instance))),
-//         StreamProvider<User?>(
-//           create: (context) => context.read<AuthRepository>().authStateChanges,
-//           initialData: null,
-//         ),
-//       ],
-//       child: MaterialApp(
-//         debugShowCheckedModeBanner: false,
-//         theme: ThemeData(
-//             scaffoldBackgroundColor: Colors.white,
-//             fontFamily: 'Roboto',
-//             primaryColor: Colors.black),
-//         home: user == null ? const SignUpScreen() : const DashboardScreen(),
-//       ),
-//     );
-//   }
-// }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
