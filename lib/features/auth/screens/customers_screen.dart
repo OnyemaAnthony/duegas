ddcd129b4b7757720893b83c143ff6cab88429f9
OnyemaAnthony <Anthonychibuike246@gmail.com>
2025-08-02 20:36:03 +0100

    fix customers screen

--- lib/features/auth/screens/customers_screen.dart
+++ lib/features/auth/screens/customers_screen.dart
@@ -1,10 +1,38 @@
+import 'package:duegas/features/app/screens/user_profile.dart';
+import 'package:duegas/features/auth/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CustomersScreen extends StatelessWidget {
   const CustomersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body:
+          Consumer<AuthenticationProvider>(builder: (context, provider, child) {
+        if (provider.isLoading) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  itemCount: provider.customers?.length,
+                  itemBuilder: (context, index) {
+                    return buildCustomerListItem(
+                        context, provider.customers![index]);
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
+      }),
+    );
   }
 }
